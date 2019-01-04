d946a351150a559e763acf041be8174613cb65f8
Obilarius <git@walzenbach.com>
2019-01-04 08:33:53 +0100

    Second Rest Api Tutorial

--- README.md
+++ README.md
@@ -8,11 +8,14 @@ If you want to learn how to build this yourself you can check out the YouTube vi
 - [Learn to code a to-do list app in JavaScript - Part 2](https://www.youtube.com/watch?v=bGLZ2pwCaiI)
 
 - [NodeJS REST API: Super Simple Setup - Express & NPM (Ep1)](https://www.youtube.com/watch?v=F7NVpxxmmgM) 
+- [NodeJS REST API: Connecting to MySQL (Ep 2)](https://www.youtube.com/watch?v=mLLUqMpf5H0)
+- [NodeJS REST: Serve HTML Files and POST Requests (Ep 3)](https://www.youtube.com/watch?v=1qH-3SIFmXw&list=PL0dzCUj1L5JE4w_OctDGyZOhML6OtJSqR&index=3)
 
 ## NPM Modules
 - [Express]()
 - [nodemon]() - Use "nodemon app.js" to run the Server
 - [morgan]() - For Debuginfo in console. ("::1 - GET /users HTTP/1.1 304 - - 0.396 ms")
+- [mysql]()
 
 ## License
 [MIT](LICENSE.md) © [Sascha Walzenbach](https://walzenbach.com)
--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
 
+// Use morgan to get debuglogs
 app.use(morgan('short'))
 
 app.get('/', (req, res) => {
@@ -17,6 +19,31 @@ app.get('/users', (req, res) => {
     res.json([user1, user2])
 
     // res.send('Nodemon auto updates when I save this file')
+})
+
+app.get('/movie/:id', (req, res) => {
+    // console.log("Fetching user with id: " + req.params.id)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'moviedb',
+        password: 'moviedb',
+        database: 'moviedb'
+    })
+
+    const movieId = req.params.id
+    const queryString = 'SELECT * FROM movie WHERE id = ?'
+    connection.query(queryString, [movieId], (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query the Database: " + err)
+            res.sendStatus(500)
+            res.end()
+            return
+        }
+
+        console.log("I think we fetched movies successfully")
+        res.json(rows)
+    })
 })
 
 // localhost:3000
