6b85e336dd69d25a1b05efc44b1c1228be18561c
Obilarius <git@walzenbach.com>
2019-01-04 09:42:52 +0100

    Third Rest Api Tutorial

--- README.md
+++ README.md
@@ -10,12 +10,14 @@ If you want to learn how to build this yourself you can check out the YouTube vi
 - [NodeJS REST API: Super Simple Setup - Express & NPM (Ep1)](https://www.youtube.com/watch?v=F7NVpxxmmgM) 
 - [NodeJS REST API: Connecting to MySQL (Ep 2)](https://www.youtube.com/watch?v=mLLUqMpf5H0)
 - [NodeJS REST: Serve HTML Files and POST Requests (Ep 3)](https://www.youtube.com/watch?v=1qH-3SIFmXw&list=PL0dzCUj1L5JE4w_OctDGyZOhML6OtJSqR&index=3)
+- [NodeJS REST: How to Refactor Code into Multiple Files with Router (Ep 4)](https://www.youtube.com/watch?v=TQU6BWtc3yc&index=4&list=PL0dzCUj1L5JE4w_OctDGyZOhML6OtJSqR)
 
 ## NPM Modules
-- [Express]()
-- [nodemon]() - Use "nodemon app.js" to run the Server
-- [morgan]() - For Debuginfo in console. ("::1 - GET /users HTTP/1.1 304 - - 0.396 ms")
-- [mysql]()
+- [Express](https://www.npmjs.com/package/express)
+- [nodemon](https://www.npmjs.com/package/nodemon) - Use "nodemon app.js" to run the Server
+- [morgan](https://www.npmjs.com/package/morgan) - For Debuginfo in console. ("::1 - GET /users HTTP/1.1 304 - - 0.396 ms")
+- [mysql](https://www.npmjs.com/package/mysql)
+- [body-parser](https://www.npmjs.com/package/body-parser) - Need to get the Content of POST Request
 
 ## License
 [MIT](LICENSE.md) © [Sascha Walzenbach](https://walzenbach.com)
--- app.js
+++ app.js
@@ -4,32 +4,66 @@ const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Use static html Site in App
+app.use(express.static('./public'))
+
 // Use morgan to get debuglogs
 app.use(morgan('short'))
+
+function getConnection() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'moviedb',
+        password: 'moviedb',
+        database: 'moviedb'
+    })
+}
+
+app.post('/movie_create', (req, res) => {
+    console.log("ID: " + req.body.create_id + " - Title: " + req.body.create_title)
+    const id = req.body.create_id
+    const title = req.body.create_title
+
+    const queryString = "INSERT INTO movie (id, title) VALUES (?, ?)"
+    getConnection().query(queryString, [id, title], (err, result, fields) => {
+        if (err) {
+            console.log("Failed to insert new movie: " + err)
+            res.sendStatus(500)
+            return
+        }
+
+        console.log("Inserted an new movie with id: ", id)
+        res.end()
+    })
+})
 
 app.get('/', (req, res) => {
     console.log('Responding to root route')
     res.send('Hello from ROOOOT')
 })
 
-app.get('/users', (req, res) => {
-    var user1 = {firstName: "Sascha", lastName: "Walzenbach"}
-    var user2 = {firstName: "Peter", lastName: "Laup"}
+app.get('/movies', (req, res) => {
+    const connection = getConnection()
 
-    res.json([user1, user2])
-
-    // res.send('Nodemon auto updates when I save this file')
+    const queryString = 'SELECT * FROM movie'
+    connection.query(queryString, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query the Database: " + err)
+            res.sendStatus(500)
+            res.end()
+            return
+        }
+        res.json(rows)
+    })
 })
 
 app.get('/movie/:id', (req, res) => {
     // console.log("Fetching user with id: " + req.params.id)
 
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'moviedb',
-        password: 'moviedb',
-        database: 'moviedb'
-    })
+    const connection = getConnection()
 
     const movieId = req.params.id
     const queryString = 'SELECT * FROM movie WHERE id = ?'
